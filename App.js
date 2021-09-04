027c8a46c6b25fe8bf493cc113c7ce39f1392cfb
MarcosVel <marcos.veloso@sempreceub.com>
2021-09-04 19:13:26 -0300

    add Header e images

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import Header from './src/Header';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar barStyle='dark-content' backgroundColor='transparent' />
+      <Header />
     </View>
   );
 }
@@ -14,8 +14,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
